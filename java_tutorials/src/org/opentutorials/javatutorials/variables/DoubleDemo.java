e732d89bd87d43cb69f1a51449c007894be1d898
yunhalee <yunhalee@192.168.219.105>
2020-12-03 12:49:50 +0900

    DoubleDemo

---
+++ java_tutorials/src/org/opentutorials/javatutorials/variables/DoubleDemo.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.variables;
+
+public class DoubleDemo {
+	public static void main(String [] arg) {
+		double a;
+		a = 1.1;
+		System.out.println(a+1.1);
+		a = 2.1;
+		System.out.println(a+1.1);
+	}
+
+}
