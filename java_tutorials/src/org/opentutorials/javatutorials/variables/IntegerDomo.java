5db27e73814c0a3a4e7d285c9929db93bcf9f68c
yunhalee <yunhalee@192.168.219.105>
2020-12-03 12:44:38 +0900

    IntegerDemo

---
+++ java_tutorials/src/org/opentutorials/javatutorials/variables/IntegerDomo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntegerDomo {
+	public static void main(String [] arg) {
+		int a;//데이터 형식(int), 변수의 이름(a)---변수 선언
+		a = 1;//대입
+		System.out.println(a+1);//2
+		a=2;
+		System.out.println(a+1);//3
+		
+	}
+
+}
