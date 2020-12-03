9ea31222de40a8a00a19fdf84cb3723e9021c5d0
yunhalee <yunhalee@192.168.219.105>
2020-12-03 12:54:37 +0900

    StringDemo

---
+++ java_tutorials/src/org/opentutorials/javatutorials/variables/StringDemo.java
@@ -0,0 +1,19 @@
+package org.opentutorials.javatutorials.variables;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String first;
+		first = "coding";
+		
+		System.out.println(first+" "+"everybody");
+		
+		String a, b;
+		a = "coding";
+		b = " everybody";
+		System.out.println(a+b);
+		
+		
+	}
+
+}
