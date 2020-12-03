3406fbf4a640de0712b6f7441f977e90b06d70ba
yunhalee <yunhalee@192.168.219.105>
2020-12-03 12:37:57 +0900

    CharString

---
+++ java_tutorials/src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+	public static void main(String []arg) {
+		System.out.println('생');
+		System.out.println("생활코딩");
+		System.out.println("생");
+		System.out.println("생활코딩"+"입니다");
+		System.out.println("생활"+"코딩");
+		System.out.println("1"+"1");
+		//문자열 안에 따옴표 넣고 싶을 때 역슬래쉬 사용 \"
+		System.out.println("egoing said \"Welcome Programming World\"");
+		//문자열 줄바꿈 하고 싶을 때 역슬래쉬n 사용하면 된다 \n
+		System.out.println("HTML\nCSS\nJavaScript\n");
+
+	}
+}
