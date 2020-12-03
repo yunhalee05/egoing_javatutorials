5618559e7d6f462c189c63fb5686de218029cd11
yunhalee <yunhalee@192.168.219.105>
2020-12-03 12:25:16 +0900

    NumberString

---
+++ java_tutorials/src/org/opentutorials/javatutorials/numberstring/Number.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println(1+2);
+		System.out.println(1.2+1.3);
+		System.out.println(2*5);
+		System.out.println(6/2);
+	}
+}
