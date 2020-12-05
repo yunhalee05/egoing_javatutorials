72134d7e7ef93d30a12a8841e61f106d66dafe5c
yunhalee05 <yunhalee0506@naver.com>
2020-12-05 21:29:35 +0900

    Array

--- java_tutorials/src/org/opentutorials/javatutorials/array/DefineDemo.java
+++ java_tutorials/src/org/opentutorials/javatutorials/array/DefineDemo.java
@@ -3,6 +3,7 @@ package org.opentutorials.javatutorials.array;
 public class DefineDemo {
 	public static void main(String []args) {
 		String[] classGroup = { "최진혁", "최유빈", "한이람", "이고잉" };
+		//[]가 배열나타내준다."" 는 문자열을 나타낸다.
 	}
 
 }
--- java_tutorials/src/org/opentutorials/javatutorials/array/ForEachDemo.java
+++ java_tutorials/src/org/opentutorials/javatutorials/array/ForEachDemo.java
@@ -3,8 +3,35 @@ package org.opentutorials.javatutorials.array;
 public class ForEachDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 String[] members = { "최진혁", "최유빈", "한이람" };
+	        for (String e : members) {
+	            System.out.println(e + "이 상담을 받았습니다");
+	        }
+	        
+	        //for문에서 더 간편하게 String e에 각각의 멤버 요소를 담아서 실행한다.
+	       //반복문과 배열은 뗄 수 없는 관계다.
+	        
+	        
+	        
+	        /*String[] members = { "최진혁", "최유빈", "한이람" };
+	        System.out.println(members[3]);     
+	        1
+	        2
+	        Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 3
+	            at ot_array.ExceptionDemo.main(ExceptionDemo.java:7)
+	         */
+	        
+	        
+	        
+	        /*String[] members = new String[3];
+	        members[0] = "최진혁";
+	        members[1] = "최유빈";
+	        members[2] = "한이람";
+	        members[3] = "이고잉";
+	        
+	        
+	        //할당받지 않은 자리에 값 넣으려고하면 오류발생한다.
+	        */
 	}
 
 }
