0a3c897651593c58fb0d6dbafc97bc3e3b348223
yunhalee05 <yunhalee0506@naver.com>
2020-12-06 13:22:32 +0900

    InputOutput

---
+++ java_tutorials/src/org/opentutorials/javatutorials/Io/InputDemo1.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.Io;
+
+public class InputDemo1 {
+	 public static void main(String[] args){
+	        System.out.println(args.length);
+	        //터미널에서 tutorials제일 상위 프로그램으로 들어간 다음에
+	        //java -cp bin org.opentutorials.javatutorials.Io.InputDemo1 1 2 3
+	        //엔터 누르면 1 2 3이 하나씩 main의 args에 들어가게 되여 결과가 3이 나온다. (length) 
+	    }
+}
--- java_tutorials/src/org/opentutorials/javatutorials/method/ReturnDemo.java
+++ java_tutorials/src/org/opentutorials/javatutorials/method/ReturnDemo.java
@@ -3,8 +3,8 @@ package org.opentutorials.javatutorials.method;
 public class ReturnDemo {
 	public static int one() {
         return 1;//제일먼저 실행되는 1이 되돌아온다. 
-        return 2;
-        return 3;//뒤의 리턴들은 실행되지 않는 코드로 컴파일 오류가 나다. 
+        //return 2;
+        //return 3;//뒤의 리턴들은 실행되지 않는 코드로 컴파일 오류가 나다. 
     }
  
     public static void main(String[] args) {
