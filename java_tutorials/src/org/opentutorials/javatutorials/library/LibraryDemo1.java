9d8f31129d429bdae459b5aae73f298bc6d51331
yunhalee05 <yunhalee0506@naver.com>
2020-12-09 12:01:59 +0900

    API

---
+++ java_tutorials/src/org/opentutorials/javatutorials/library/LibraryDemo1.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.library;
+
+public class LibraryDemo1 {
+    public static void main(String[] args) {
+        System.out.println(1);//여기서 System은 클래스고, out은 그 안의 필드, println은 메소다.
+    }
+}
+
+/*
+ 아래의 코드와 같다. 
+ package org.opentutorials.javatutorials.library;
+import java.lang.*;자바, 랭 패키지의 모든 것 로드(임포트 따로 안써줘도 자바가 알아서 얘를 로드하기 때문에 그냥 써도 작동하는 것이다. )
+public class LibraryDemo1 {
+    public static void main(String[] args) {
+        System.out.println(1); 	시스템은 자바 랭의 소속이다. 
+    }
+}
+ */
